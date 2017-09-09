fd03bbecb8e6fc2cfea4d0a6def407368be445b1
Max Alva <alva85@gmail.com>
2017-09-09 10:04:46 -0500

    crud completo

--- controllers/frutaCtrl.js
+++ controllers/frutaCtrl.js
@@ -43,12 +43,77 @@ function getFrutas(req, res) {
         } else {
             res.status(404).send({
                 message: 'No existen frutas registradas'
-            });            
+            });
+        }
+    });
+}
+
+function getFruta(req, res) {
+    let frutaId = req.params.frutaId;
+    Fruta.findById(frutaId, (err, fruta) => {
+        if (err) {
+            res.status(500).send({
+                error: err,
+                message: 'Error en el servidor'
+            });
+        } else if (fruta) {
+            res.status(200).send({
+                fruta
+            });
+        } else {
+            res.status(404).send({
+                message: 'No se puede obtener la fruta solicitada'
+            });
+        }
+    });
+}
+
+function updateFruta(req, res) {
+    let frutaId = req.params.frutaId;
+    let data = req.body;
+    Fruta.findByIdAndUpdate(frutaId, data, { new: true }, (err, frutaUpdated) => {
+        if (err) {
+            res.status(500).send({
+                error: err,
+                message: 'Error en el servidor'
+            });
+        } else if (frutaUpdated) {
+            res.status(200).send({
+                fruta: frutaUpdated
+            });
+        } else {
+            res.status(404).send({
+                message: 'No existe la fruta',
+            });
+        }
+    });
+}
+
+function deleteFruta(req, res) {
+    let frutaId = req.params.frutaId;
+    Fruta.findByIdAndRemove(frutaId, (err, frutaRemoved) => {
+        if (err) {
+            res.status(500).send({
+                error: err,
+                message: 'Error en el servidor'
+            });
+        } else if (frutaRemoved) {
+            res.status(200).send({
+                fruta: frutaRemoved,
+                message: 'Producto eliminado'
+            });
+        } else {
+            res.status(404).send({
+                message: 'No existe la fruta'
+            });
         }
     });
 }
 
 module.exports = {
     saveFruta,
-    getFrutas
+    getFrutas,
+    getFruta,
+    updateFruta,
+    deleteFruta
 };
--- routes/router.js
+++ routes/router.js
@@ -7,5 +7,8 @@ const frutaCtrl = require('../controllers/frutaCtrl');
 
 api.post('/fruta', frutaCtrl.saveFruta);
 api.get('/frutas', frutaCtrl.getFrutas);
+api.get('/fruta/:frutaId', frutaCtrl.getFruta);
+api.put('/fruta/:frutaId', frutaCtrl.updateFruta);
+api.delete('/fruta/:frutaId', frutaCtrl.deleteFruta);
 
 module.exports = api;
